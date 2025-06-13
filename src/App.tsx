c3f9ae59351265916783de2b09c67eb5dd8a03c0
Edrick <linardiedrick@gmail.com>
2025-06-13 18:50:08 -0400

    fixed tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import SongSearchEngine from './components/SongSearchEngine'
 
 function App() {
-  return (
-    <div className="bg-red-500 text-white p-4">
-      <h1 className="text-2xl">Test - This should be red background</h1>
-      <SongSearchEngine />
-    </div>
-  )
+  return <SongSearchEngine />
 }
 
 export default App
